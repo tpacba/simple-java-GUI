29e4ef0cf162ad5eb62ace7f1d720946ce25c1d7
Tristan Pacba <tpacba@live.com>
2020-10-13 12:08:27 -0700

    imported JFrame

---
+++ src/GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GUI {
+    public GUI() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        panel.setBorder();
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
